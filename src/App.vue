6aee8862c0e7eb4b673b9c1ca4170fa835cb1cf7
Vladislav <rushella.ln@outlook.com>
2025-01-22 02:48:33 +0500

    added sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import SideBar from './components/SideBar.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    SideBar,
+  },
 })
 </script>
 
 <template>
-  <div class="app-bg-dark"></div>
+  <div class="app-bg-dark">
+    <SideBar />
+  </div>
 </template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SideBar',
+})
+</script>
+
+<template>
+  <div class="w-64 overflow-y-scroll bg-gray-900 rounded-md">
+    <div class="px-6 pt-8">
+      <div class="flex items-center justify-between">
+        <a class="font-bold tracking-[.050em] lg:text-4xl text-slate-500">EPHEMERIS</a>
+      </div>
+    </div>
+    <div class="px-6 pt-4">
+      <hr class="border-gray-700" />
+    </div>
+  </div>
+</template>
