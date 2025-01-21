5beea82a227a229af90a46dcf317f17dea204785
Vladislav <rushella.ln@outlook.com>
2025-01-22 03:29:11 +0500

    sidebar refactoring into vue components

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,8 +1,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import SideBarItem from './SideBarItem.vue'
 
 export default defineComponent({
   name: 'SideBar',
+  components: {
+    SideBarItem,
+  },
 })
 </script>
 
@@ -10,11 +14,17 @@ export default defineComponent({
   <div class="w-64 overflow-y-scroll bg-gray-900 rounded-md">
     <div class="px-6 pt-8">
       <div class="flex items-center justify-between">
-        <a class="font-bold tracking-[.050em] lg:text-4xl text-slate-500">EPHEMERIS</a>
+        <a class="font-bold tracking-[.040em] text-4xl text-slate-500">EPHEMERIS</a>
       </div>
     </div>
     <div class="px-6 pt-4">
       <hr class="border-gray-700" />
+    </div>
+    <div class="px-6 pt-4">
+      <ul class="flex flex-col space-y-2">
+        <SideBarItem name="AUDIO" />
+        <SideBarItem name="MESSAGES" />
+      </ul>
     </div>
   </div>
 </template>
---
+++ src/components/SideBarItem.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SideBarItem',
+  props: {
+    name: {
+      required: true,
+      type: String,
+    },
+  },
+})
+</script>
+
+<template>
+  <li class="side-bar-item">
+    <a href="#" class="side-bar-item-link">{{ name }} </a>
+  </li>
+</template>
